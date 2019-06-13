58dbb7f4cc343121974153c209d430760f5524ff
Becky Phan <beckyphan@live.com>
2019-06-13 02:48:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,20 +20,20 @@ class Scraper
     self.get_page.css(".post")
   end
 
-  def make_courses
-
-  end
-
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+  # def make_courses
+  # 
+  # end
+  # 
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
+  # end
 
 end
 
